938b90befdecb1255e11bc0053ff89eeb5cd6878
Pedro Merlo Jr <pedro_merlo@globo.com>
2017-04-20 16:24:46 -0300

    Criação da estrutura de contas a receber

---
+++ db/migrations/20170420190314_create_bill_recive_table.php
@@ -0,0 +1,23 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class CreateBillReciveTable extends AbstractMigration
+{
+
+    public function up(){
+        $this->table('bill_recives')
+            ->addColumn('date_launch','date')
+            ->addColumn('name', 'string')
+            ->addColumn('value','float' )
+            ->addColumn('user_id','integer' )
+            ->addColumn('created_at','datetime')
+            ->addColumn('updated_at','datetime')
+            ->addForeignKey('user_id','users','id')
+            ->save();
+
+    }
+    public function down(){
+        $this->dropTable('bill_recives');
+    }
+}
